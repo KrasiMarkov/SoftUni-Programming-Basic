5b246e69f0a1ea983c28d960000ae426653ab2ea
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2021-07-10 21:00:09 +0300

    Add files via upload

---
+++ areaAB/areaAB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace areaAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine(7 * 2);
+        }
+
+    }
+}
+
