4386970ed0355ea19728d38a4575c9908912e015
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2021-07-24 19:26:36 +0300

    Add files via upload

---
+++ Radians to Degrees/Radians to Degrees/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Radians_to_Degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad = double.Parse(Console.ReadLine());
+            double degrees = rad * (180 / Math.PI);
+            Console.WriteLine(Math.Round(degrees));
+        }
+    }
+}
