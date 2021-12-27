40fc3b15a270fa21150c4705bc0c3b85aa9c03a9
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2021-12-27 10:26:14 +0200

    Add files via upload

---
+++ Greeting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", firstName);
+        }
+    }
+}
