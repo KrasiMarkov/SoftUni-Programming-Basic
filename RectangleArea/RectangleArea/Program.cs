9bb516835b0d88c7f6118f2751fd4a7339b641c4
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-01-16 22:56:29 +0200

    Add files via upload

---
+++ RectangleArea/RectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double X1 = double.Parse(Console.ReadLine());
+            double Y1 = double.Parse(Console.ReadLine());
+            double X2 = double.Parse(Console.ReadLine());
+            double Y2 = double.Parse(Console.ReadLine());
+            double length = Math.Abs(X1 - X2);
+            double wigth = Math.Abs(Y1 - Y2);
+            double area = length * wigth;
+            double perimeter = 2 * (length + wigth);
+            Console.WriteLine($"{area:F2}");
+            Console.WriteLine($"{perimeter:F2}");
+        }
+    }
+}
