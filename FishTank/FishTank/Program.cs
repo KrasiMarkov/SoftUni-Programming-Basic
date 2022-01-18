af8eeb296e8afd3ab987e621c0c7828e7b082733
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-01-18 23:43:20 +0200

    Add files via upload

---
+++ FishTank/FishTank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double wigth = double.Parse(Console.ReadLine());
+            double higth = double.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            double volume = length * wigth * higth;
+            double volumeInLiters = volume * 0.001;
+            double sand = procent * 0.01;
+            double realLiters = volumeInLiters * (1 - sand);
+            Console.WriteLine($"{realLiters:F3}");
+
+        }
+    }
+}
