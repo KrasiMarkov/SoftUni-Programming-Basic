971846b0f9f2aacbf276139b36b0d3cc419dd730
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-01-19 22:09:55 +0200

    Add files via upload

---
+++ Dance Hall/Dance Hall/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dance_Hall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double L = double.Parse(Console.ReadLine());
+            double W = double.Parse(Console.ReadLine());
+            double A = double.Parse(Console.ReadLine());
+            double areaHall = (L * 100) * (W * 100);
+            double areaSquare = (A * 100) * (A * 100);
+            double bench = areaHall / 10;
+            double freeSpace = areaHall - areaSquare - bench;
+            double numDancer = freeSpace / (40 + 7000);
+            Console.WriteLine(Math.Floor(numDancer));
+        }
+    }
+}
