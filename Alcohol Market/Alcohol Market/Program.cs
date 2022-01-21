ccbda105c2cd48a097a5f692d7147d516c928267
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-01-21 23:38:52 +0200

    Add files via upload

---
+++ Alcohol Market/Alcohol Market/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Alcohol_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            double PriceWiskey = double.Parse(Console.ReadLine());
+            double QuantityBeer = double.Parse(Console.ReadLine());
+            double QuantityWine = double.Parse(Console.ReadLine());
+            double QuantityRakia = double.Parse(Console.ReadLine());
+            double QuantityWiskey = double.Parse(Console.ReadLine());
+
+            //Logic
+            double PriceRakia = PriceWiskey / 2;
+            double PriceWine = PriceRakia - (0.4 * PriceRakia);
+            double PriceBeer = PriceRakia - (0.8 * PriceRakia);
+
+            double SumRakia = QuantityRakia * PriceRakia;
+            double SumWine = QuantityWine * PriceWine;
+            double SumBeer = QuantityBeer * PriceBeer;
+            double SumWiskey = QuantityWiskey * PriceWiskey;
+
+            double EndSum = SumRakia + SumWine + SumBeer + SumWiskey;
+
+            //Output
+            Console.WriteLine($"{EndSum:F2}");
+        }
+    }
+}
