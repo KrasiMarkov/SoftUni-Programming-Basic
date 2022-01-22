81c55f8908f7936011aa04cbcf391af703207e17
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-01-22 20:31:51 +0200

    Add files via upload

---
+++ Tailoring Workshop/Tailoring Workshop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tailoring_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numTable = double.Parse(Console.ReadLine());
+            double lengthTable = double.Parse(Console.ReadLine());
+            double wigthTable = double.Parse(Console.ReadLine());
+            double areaP = numTable * (lengthTable + 2 * 0.30) * (wigthTable + 2 * 0.30);
+            double areaK = numTable * (lengthTable / 2) * (lengthTable / 2);
+            double PriceUsd = areaP * 7 + areaK * 9;
+            double PriceBgn = PriceUsd * 1.85;
+            Console.WriteLine($"{PriceUsd:F2} USD");
+            Console.WriteLine($"{PriceBgn:F2} BGN");
+        }
+    }
+}
