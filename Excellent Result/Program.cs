fdae03d6327528c88d1009cda62bff9ca19cacbd
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-08 23:26:57 +0200

    Add files via upload

---
+++ Excellent Result/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Number = double.Parse(Console.ReadLine());
+            if (Number >= 5.50)
+            {
+              Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
