648da0f8f6e080ad534879b219e8007e69982151
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-08 23:26:25 +0200

    Add files via upload

---
+++ Even or Odd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = int.Parse(Console.ReadLine());
+            if(Number % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+
+        }
+    }
+}
