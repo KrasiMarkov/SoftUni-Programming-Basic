f47a33fd4a7656858c02cd928c12a22e553bbf99
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-10 23:33:52 +0200

    Add files via upload

---
+++ Greater Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Greater_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNumber = int.Parse(Console.ReadLine());
+            int SecondNumber = int.Parse(Console.ReadLine());
+            if (FirstNumber > SecondNumber)
+            {
+                Console.WriteLine(FirstNumber);
+            }
+            else 
+            {
+                Console.WriteLine(SecondNumber);
+            }
+
+
+        }
+    }
+}
