683f480378138e94f95000ba9cbf6b59be4d5f48
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-10 23:35:29 +0200

    Add files via upload

---
+++ Number 100 to 200/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Number_100_to_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = int.Parse(Console.ReadLine());
+            if (Number < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (Number <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else if (Number > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+
+        }   
+    }
+}
