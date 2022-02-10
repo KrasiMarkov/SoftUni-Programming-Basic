5e4f3ee49f3f7c8bf94d5d473ed53d4f415e8a3c
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-10 23:34:53 +0200

    Add files via upload

---
+++ Number 1 to 9 to Text/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Number_1_to_9_to_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = int.Parse(Console.ReadLine());
+            if(Number == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else if(Number == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else if(Number == 3)
+            {
+                Console.WriteLine("three");
+            }
+            else if(Number == 4)
+            {
+                Console.WriteLine("four");
+            }
+            else if(Number == 5)
+            {
+                Console.WriteLine("five");
+            }
+            else if(Number == 6)
+            {
+                Console.WriteLine("six");
+            }
+            else if(Number == 7)
+            {
+                Console.WriteLine("seven");
+            }
+            else if(Number == 8)
+            {
+                Console.WriteLine("eight");
+            }
+            else if(Number == 9)
+            {
+                Console.WriteLine("nine");
+            }
+            else if(Number > 9)
+            {
+                Console.WriteLine("number too big");
+            }
+
+        }
+    }
+}
