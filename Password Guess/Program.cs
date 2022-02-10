35236da02469450ddc20b2872c9474fe00e391ec
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-10 23:36:04 +0200

    Add files via upload

---
+++ Password Guess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Password_Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Password = Console.ReadLine();
+            string MyPassword = "s3cr3t!P@ssw0rd";
+            if (Password == MyPassword)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
