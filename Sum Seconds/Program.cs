e419758cb9a1cf34ad9b432c0becf2ba49966e3b
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-13 23:34:28 +0200

    Add files via upload

---
+++ Sum Seconds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstTime = int.Parse(Console.ReadLine());
+            int SecondTime = int.Parse(Console.ReadLine());
+            int ThirdTime = int.Parse(Console.ReadLine());
+
+            int TotalTime = FirstTime + SecondTime + ThirdTime;
+            int minutes = TotalTime / 60;
+            int seconds = TotalTime % 60;
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+
+        }
+    }
+}
