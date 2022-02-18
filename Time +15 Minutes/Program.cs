87ca7287a0fd30457dce5cc9aabc92a1238e7ab6
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-18 23:34:31 +0200

    Add files via upload

---
+++ Time +15 Minutes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Time__15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Hour = int.Parse(Console.ReadLine());
+            int Minutes = int.Parse(Console.ReadLine());
+
+            Minutes = Minutes + 15;
+
+            if (Minutes > 59)
+            {
+                Hour = Hour + 1;
+                Minutes = Minutes - 60;
+            }
+            if (Hour > 23)
+            {
+                Hour = 0;
+            }
+            if (Minutes < 10)
+            {
+                Console.WriteLine($"{Hour}:0{Minutes}");
+            }
+            else
+            { 
+                Console.WriteLine($"{Hour}:{Minutes}");
+            }
+        }      
+    }
+}
