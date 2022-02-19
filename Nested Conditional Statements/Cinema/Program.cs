0c9dee7c5ac8496825784b45cf20c5742704ee85
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-19 22:56:53 +0200

    Add files via upload

---
+++ Nested Conditional Statements/Cinema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int row = int.Parse(Console.ReadLine());
+            int column = int.Parse(Console.ReadLine());
+
+            double priceForPremiere = 12.00;
+            double priceForNormal = 7.50;
+            double priceForDiscount = 5.00;
+
+            if (projection == "Premiere")
+            {
+                double result = priceForPremiere * row * column;
+                Console.WriteLine($"{result:F2} leva");
+            }
+            else if (projection == "Normal")
+            {
+                double result = priceForNormal * row * column;
+                Console.WriteLine($"{result:F2} leva");
+            }
+            else if (projection == "Discount")
+            {
+                double result = priceForDiscount * row * column;
+                Console.WriteLine($"{result:F2} leva");
+            }
+        }
+    }
+}
