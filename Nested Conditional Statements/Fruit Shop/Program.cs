12a2c5fef6a2772f10bfcf0a5486cbe2b2326034
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-21 23:04:01 +0200

    Add files via upload

---
+++ Nested Conditional Statements/Fruit Shop/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Fruit_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")
+            {
+                if (fruit == "banana")
+                {
+                    double price = 2.50;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "apple")
+                {
+                    double price = 1.20;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "orange")
+                {
+                    double price = 0.85;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "grapefruit")
+                {
+                    double price = 1.45;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "kiwi")
+                {
+                    double price = 2.70;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "pineapple")
+                {
+                    double price = 5.50;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "grapes")
+                {
+                    double price = 3.85;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                if (fruit == "banana")
+                {
+                    double price = 2.70;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "apple")
+                {
+                    double price = 1.25;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "orange")
+                {
+                    double price = 0.90;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "grapefruit")
+                {
+                    double price = 1.60;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "kiwi")
+                {
+                    double price = 3.00;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "pineapple")
+                {
+                    double price = 5.60;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else if (fruit == "grapes")
+                {
+                    double price = 4.20;
+                    Console.WriteLine($"{(price * quantity):F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+
+           
+        }
+    }
+}
