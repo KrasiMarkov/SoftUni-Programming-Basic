28c743bb3d3e8e1f90d29f498e1f2b9f93ff491a
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2022-02-22 23:26:55 +0200

    Add files via upload

---
+++ Nested Conditional Statements/Invalid Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if ((number != 0) && (number <= 99) || (number >= 201))
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
