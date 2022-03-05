1eae93c4f21105e184bbc978ec3d633abcb25983
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-06 00:34:26 +0200

    Add files via upload

---
+++ WhileLoops/Account Balance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfOperations = int.Parse(Console.ReadLine());
+            int counter = 0;
+            double total = 0;
+            while (counter < numOfOperations)
+            {
+                double deposit = double.Parse(Console.ReadLine());
+                if (deposit < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {deposit:f2}");
+                total = total + deposit;
+                counter++;
+            }
+            Console.WriteLine($"Total: {total:F2}");
+
+
+
+        }
+    }
+}
