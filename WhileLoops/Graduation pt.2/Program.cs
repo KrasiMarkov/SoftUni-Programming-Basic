5323264ee36225689ecd3c20f4a130c8490459ac
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-07 23:03:00 +0200

    Add files via upload

---
+++ WhileLoops/Graduation pt.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Graduation_pt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int counter = 0;
+            int n = 12;
+            double averageGrade = 0;
+            double totalAverageGrade = 0;
+            int excluded = 0;
+            bool isExcluded = false;
+
+            while (counter < n)
+            {
+
+                double annualAssessment = double.Parse(Console.ReadLine());
+                if (annualAssessment < 4.00)
+                {
+
+                    excluded++;
+                    if (excluded == 2)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {counter} grade");
+                        isExcluded = true;
+                        break;
+                    }
+                    counter++;
+                }
+                else if (annualAssessment >= 4.00)
+                {
+                    averageGrade = averageGrade + annualAssessment;
+                    counter++;
+                }
+
+
+            }
+            if (isExcluded == false)
+            {
+                totalAverageGrade = averageGrade / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {totalAverageGrade:F2}");
+            }
+           
+
+        }
+    }
+}
