466c8a56e08c98aa24e166e5b6920bf042412f02
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-07 23:03:34 +0200

    Add files via upload

---
+++ WhileLoops/Max Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int maxNum = int.MinValue;
+            while (counter < n)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+                counter++;
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
