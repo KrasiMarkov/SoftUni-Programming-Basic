cd9e508201a5b300f6a4cf400415d7bec8243de1
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-07 23:02:22 +0200

    Add files via upload

---
+++ WhileLoops/Graduation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int counter = 0;
+            int n = 12;
+            double averageGrade = 0;
+            double totalAverageGrade = 0;
+            while (counter < n)
+            {
+
+                double annualAssessment = double.Parse(Console.ReadLine());
+                if (annualAssessment < 4.00)
+                {
+                    
+                    continue;
+                }
+                averageGrade = averageGrade + annualAssessment;
+                counter++;
+
+            }
+            totalAverageGrade = averageGrade / 12;
+            Console.WriteLine($"{name} graduated. Average grade: {totalAverageGrade:F2}");
+        }
+    }
+}
