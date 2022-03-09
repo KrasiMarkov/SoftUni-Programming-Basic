bfad1be99b1a22616a9b355396fa0596f84346aa
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-09 23:56:53 +0200

    Add files via upload

---
+++ WhileLoops/Moving/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            
+            
+            int freeSpace = width * length * height;
+            string command = Console.ReadLine();
+            while (command != "Done")
+            {
+
+                int boxes = int.Parse(command);
+                if (freeSpace >= boxes)
+                {
+                    freeSpace = freeSpace - boxes;
+                }
+                else
+                {
+                    Console.WriteLine($"No more free space! You need {boxes - freeSpace} Cubic meters more.");
+                    break;
+                }
+                command = Console.ReadLine();
+
+            }
+            if (command == "Done")
+            { 
+                Console.WriteLine($"{freeSpace} Cubic meters left.");
+            }
+            
+        }
+    }
+}
