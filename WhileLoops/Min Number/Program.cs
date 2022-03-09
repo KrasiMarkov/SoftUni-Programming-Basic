ab7f04d1967e5c94e0871deff04ccbbb8cba1625
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-09 23:56:22 +0200

    Add files via upload

---
+++ WhileLoops/Min Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int minNum = int.MaxValue;
+            while (counter < n)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < minNum)
+                {
+                    minNum = number;
+                }
+                counter++;
+
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
