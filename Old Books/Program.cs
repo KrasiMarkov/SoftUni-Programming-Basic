4e33e42c192cdd0c8fd030164bf55dac22e41002
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-13 23:16:28 +0200

    Add files via upload

---
+++ Old Books/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string loveBook = Console.ReadLine();
+            int capacity = int.Parse(Console.ReadLine());
+            string othersBook = Console.ReadLine();
+            int counter = 0;
+            while (othersBook != loveBook)
+            {
+                if (counter < capacity)
+                {
+                    othersBook = Console.ReadLine();
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {capacity} books.");
+                    break;
+                }
+
+            }
+            if (othersBook == loveBook)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+        }
+    }
+}
