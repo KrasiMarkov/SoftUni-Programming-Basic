ca054b44699250cd2cf08ce258de737b3902efea
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-13 23:16:02 +0200

    Add files via upload

---
+++ WhileLoops/Number In Range/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Number_In_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            while (num < 1 || num > 100)
+            {
+                Console.WriteLine("Invalid number!");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {num}");
+        }
+    }
+}
