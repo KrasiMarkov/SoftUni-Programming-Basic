2755befc70585ec2edcb225918cb25d51125b20b
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-23 23:25:31 +0200

    Add files via upload

---
+++ For Loops/Left and Right Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                leftSum = leftSum + number;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                rightSum = rightSum + number;
+            }
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+            }
+        }
+    }
+}
