651fc453478c828e3ee8c9b931184ca65ff5e36d
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-26 19:35:08 +0200

    Add files via upload

---
+++ For Loops/Odd Even Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumEven = 0;
+            int sumOdd = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    sumEven = sumEven + number;
+                }
+                else 
+                {
+                    sumOdd = sumOdd + number;
+                }
+            }
+            if (sumEven == sumOdd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumEven}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sumEven - sumOdd)}");
+            }
+        }
+    }
+}
