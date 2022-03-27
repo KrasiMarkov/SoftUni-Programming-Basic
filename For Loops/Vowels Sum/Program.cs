37fe213a06a338c97f658640489054b7694578c9
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-27 23:50:08 +0300

    Add files via upload

---
+++ For Loops/Vowels Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char current = text[i];
+                switch (current)
+                {
+                    case 'a':
+                        sum = sum + 1;
+                        break;
+                    case 'e':
+                        sum = sum + 2;
+                        break;
+                    case 'i':
+                        sum = sum + 3;
+                        break;
+                    case 'o':
+                        sum = sum + 4;
+                        break;
+                    case 'u':
+                        sum = sum + 5;
+                        break;
+
+
+
+
+
+
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
