076caacba14640ce7d1d29b6ccc3be1105307beb
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-30 23:04:29 +0300

    Add files via upload

---
+++ Nested Loops/Building/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numFloor = int.Parse(Console.ReadLine());
+            int numRoom = int.Parse(Console.ReadLine());
+
+            for (int i = numFloor; i >= 1; i--)
+            {
+                for (int j = 0; j < numRoom; j++)
+                {
+                    if (numFloor == i)
+                    {
+                        Console.Write($"L{i}{j} ");
+                    }
+
+
+
+                    else if (i % 2 == 0)
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                    else if (i % 2 != 0)
+                    {
+                        Console.Write($"A{i}{j} ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
