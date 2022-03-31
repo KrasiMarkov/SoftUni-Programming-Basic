80af1aaa41e4d7b55bdfa9b54c3fdb18208524c0
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-03-31 22:28:48 +0300

    Add files via upload

---
+++ Cookie factorie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cookie_factorie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
