fe3fdb47da0f7f86deb945db5b07d4efea7031a6
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-01 22:29:49 +0300

    Add files via upload

---
+++ Nested Loops/Matrix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+        }
+    }
+}
