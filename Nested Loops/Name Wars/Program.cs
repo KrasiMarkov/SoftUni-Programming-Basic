bf385fe800fd5f9324cf437cac957fd299055813
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-03 22:45:02 +0300

    Add files via upload

---
+++ Nested Loops/Name Wars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Name_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int sum = 0;
+            int totalSum = 0;
+            string name2 = "";
+            while (name != "STOP")
+            {
+                for (int i = 0; i < name.Length; i++)
+                {
+                    char symbol = name[i];
+                    sum = sum + ((int)symbol);
+                }
+                if (totalSum < sum)
+                {
+                    totalSum = sum;
+                    name2 = name;
+
+                }
+                sum = 0;
+                name = Console.ReadLine();
+
+            }
+            Console.WriteLine($"Winner is {name2} - {totalSum}!");
+        }
+    }
+}
