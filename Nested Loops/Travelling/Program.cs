3d18111600d371fde94351b1163d93742a21d4e8
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-04 23:35:43 +0300

    Add files via upload

---
+++ Nested Loops/Travelling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            
+            double savedMoney = 0.0;
+            while (command != "End")
+            {
+                double minBudjet = double.Parse(Console.ReadLine());
+                double sumMoney = double.Parse(Console.ReadLine());
+                savedMoney = savedMoney + sumMoney;
+                while (savedMoney < minBudjet)
+                {
+
+                    sumMoney = double.Parse(Console.ReadLine());
+                    savedMoney = savedMoney + sumMoney;
+
+                }
+                Console.WriteLine($"Going to {command}!");
+                savedMoney = 0.0;
+                minBudjet = 0.0;
+                command = Console.ReadLine();
+               
+                
+            }
+            
+        }
+    }
+}
