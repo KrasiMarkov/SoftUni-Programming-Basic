2dbd0cf1d0deba2f5a3ed6482b24b7794fc48c2c
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-04 23:35:04 +0300

    Add files via upload

---
+++ Nested Loops/Sum of Two Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int combination = 0;
+            bool flag = false;
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                for (int j = startNumber; j <= endNumber; j++)
+                {
+                    sum = i + j;
+                    combination++;
+                    if (sum == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combination} ({i} + {j} = {magicNumber})");
+                        flag = true;
+                        break;
+                    }
+
+                }
+                if (flag == true)
+                {
+                    break;
+                }
+            }
+            if (sum != magicNumber)
+            {
+                Console.WriteLine($"{combination} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
