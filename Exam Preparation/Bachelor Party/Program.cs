0144671469280295063d2ff8fe3747e46304933b
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-07 23:32:00 +0300

    Add files via upload

---
+++ Exam Preparation/Bachelor Party/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Bachelor_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guest = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int sumMoney = 0;
+            int sumPeople = 0;
+            while (command != "The restaurant is full")
+            {
+                int numPeople = int.Parse(command);
+                sumPeople = sumPeople + numPeople;
+
+                if (numPeople < 5)
+                {
+                    sumMoney = sumMoney + numPeople * 100;
+                }
+                else
+                {
+                    sumMoney = sumMoney + numPeople * 70;
+                }
+
+                command = Console.ReadLine();
+            }
+            if (sumMoney >= guest)
+            {
+                Console.WriteLine($"You have {sumPeople} guests and {sumMoney - guest} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"You have {sumPeople} guests and {sumMoney} leva income, but no singer.");
+            }
+
+        }
+    }
+}
