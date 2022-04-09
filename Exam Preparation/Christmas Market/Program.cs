d4a9d56ec60b05f3ff8869d2e9ecd542ae15318d
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-09 16:44:41 +0300

    Add files via upload

---
+++ Exam Preparation/Christmas Market/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Christmas_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int numFantasyBooks = int.Parse(Console.ReadLine());
+            int numHororBooks = int.Parse(Console.ReadLine());
+            int numRomanticsBooks = int.Parse(Console.ReadLine());
+
+            double priceFantasy = 14.90;
+            double priceHoror = 9.80;
+            double priceRomantics = 4.30;
+            double totalmoney = numFantasyBooks * priceFantasy + numHororBooks * priceHoror + numRomanticsBooks * priceRomantics;
+
+            totalmoney = totalmoney - totalmoney * 0.2;
+
+
+            if (totalmoney >= money)
+            {
+
+                double profit = totalmoney - money;
+                totalmoney = totalmoney - profit;
+                double profitForSellers = Math.Floor(profit * 0.1);
+                profit = profit - profitForSellers;
+                totalmoney = totalmoney + profit;
+                Console.WriteLine($"{totalmoney:F2} leva donated.");
+                Console.WriteLine($"Sellers will receive {profitForSellers} leva.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Abs(money - totalmoney):F2} money needed.");
+            }
+
+
+        }
+    }
+}
