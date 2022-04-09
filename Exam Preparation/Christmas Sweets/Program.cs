27e96c20f88d6d913030241f4beb8f60cc4569bb
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-09 16:45:19 +0300

    Add files via upload

---
+++ Exam Preparation/Christmas Sweets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Christmas_Sweets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceBaklavaPerKg = double.Parse(Console.ReadLine());
+            double priceMuffinsPerKg = double.Parse(Console.ReadLine());
+            double stollenKg = double.Parse(Console.ReadLine());
+            double candyKg = double.Parse(Console.ReadLine());
+            int biscuitsKg = int.Parse(Console.ReadLine());
+
+
+            double priceStollenPerKg = priceBaklavaPerKg + priceBaklavaPerKg * 0.6;
+            double priceCandyPerKg = priceMuffinsPerKg + priceMuffinsPerKg * 0.8;
+            double priceBiscuitsPerKg = 7.50;
+
+            double totalPrice = priceStollenPerKg * stollenKg + priceCandyPerKg * candyKg + priceBiscuitsPerKg * biscuitsKg;
+
+
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
