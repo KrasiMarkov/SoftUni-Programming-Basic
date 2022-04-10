5c43500fd70326b64cbcffa95b5cf57dc435ac8d
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-10 23:28:06 +0300

    Add files via upload

---
+++ Exam Preparation/Movie Ratings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Movie_Ratings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numMovies = int.Parse(Console.ReadLine());
+            double maxRating = double.MinValue;
+            string movieMaxRating = "";
+            string movieMinRating = "";
+            double minRating = double.MaxValue;
+            double counter = 0.0;
+            for (int i = 1; i <= numMovies; i++)
+            {
+                string nameMovie = Console.ReadLine();
+                double ratingMovie = double.Parse(Console.ReadLine());
+
+                if (ratingMovie > maxRating)
+                {
+                    maxRating = ratingMovie;
+                    movieMaxRating = nameMovie;
+                }
+                if (ratingMovie < minRating)
+                {
+
+                    minRating = ratingMovie;
+                    movieMinRating = nameMovie;
+                }
+
+                counter = counter + ratingMovie;
+            }
+            double averageRating = counter / numMovies;
+            Console.WriteLine($"{movieMaxRating} is with highest rating: {maxRating:F1}");
+            Console.WriteLine($"{movieMinRating} is with lowest rating: {minRating:F1}");
+            Console.WriteLine($"Average rating: {averageRating:F1}");
+        }
+    }
+}
