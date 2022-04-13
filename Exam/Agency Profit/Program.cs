563f9fae7a602fd7dc1c6ffbae44f63832feec26
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-04-13 23:58:34 +0300

    Add files via upload

---
+++ Exam/Agency Profit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Agency_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numTicketsForOlder = int.Parse(Console.ReadLine());
+            int numTicketsForKids = int.Parse(Console.ReadLine());
+            double priceTicketsForOlder = double.Parse(Console.ReadLine());
+            double pricePeeTax = double.Parse(Console.ReadLine());
+
+
+            double priceTicketsForKids = priceTicketsForOlder - priceTicketsForOlder * 0.7;
+            double priceTicketsForOlderPeeTax = priceTicketsForOlder + pricePeeTax;
+            double priceTicketsForKidspeeTax = priceTicketsForKids + pricePeeTax;
+            double totalPrice = (numTicketsForKids * priceTicketsForKidspeeTax) + (numTicketsForOlder * priceTicketsForOlderPeeTax);
+            double profit = totalPrice * 0.2;
+            Console.WriteLine($"The profit of your agency from {name} tickets is {profit:F2} lv.");
+        }
+    }
+}
